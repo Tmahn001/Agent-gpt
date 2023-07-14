d0a828351a3518307992ea9a8e4e70e308839201
Tmahn001 <toluiwalewa@gmail.com>
2023-07-15 00:24:20 +0100

    npm install issues

--- setup.sh
+++ setup.sh
@@ -3,11 +3,15 @@
 # The CLI will take care of setting up the ENV variables
 cd /app/cli || exit 1
 
+# Install dependencies
+npm install
+
 # Build the Next.js app
 npm run build
 
 # Start the production server
 npm run start
+
 
 
 
