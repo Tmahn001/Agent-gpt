fec9762c88226154215bd25ba9af414f4b73698d
Tmahn001 <toluiwalewa@gmail.com>
2023-07-14 19:01:19 -0700

    f

--- setup.sh
+++ setup.sh
@@ -2,13 +2,6 @@
 
 cd "$(dirname "$0")" || exit 1
 
-# Print the current directory
-echo "Current directory: $(pwd)"
-
-# List the contents of the current directory
-echo "Directory contents:"
-ls -la
-
 # The CLI will take care of setting up the ENV variables
 cd "$(dirname "$0)"/cli || cd cli || exit 1
 npm install
